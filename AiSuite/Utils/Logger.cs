9cae331d7565731d29349c9c4b9ce7ad3df2a7b5
UmsrAkne <sibata486@gmail.com>
2026-05-21 16:35:11 +0900

    init

---
+++ AiSuite/Utils/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AiSuite.Utils
+{
+    public static class Logger
+    {
+        private readonly static string LogFileName = "log.txt";
+        private static string logFilePath;
+
+        public static void Initialize(string logPath)
+        {
+            logFilePath = Path.Combine(logPath, LogFileName);
+        }
+
+        public static void Log(string message)
+        {
+            if (string.IsNullOrWhiteSpace(logFilePath))
+            {
+                logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, LogFileName);
+            }
+
+            var line = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {message}";
+
+            Console.WriteLine(line);
+            File.AppendAllText(logFilePath, line + Environment.NewLine);
+        }
+    }
+}
---
+++ AiSuite/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using AiSuite.Utils;
+using Prism.Mvvm;
+
+namespace AiSuite.ViewModels;
+
+public class MainWindowViewModel : BindableBase
+{
+    #if DEBUG
+    // ReSharper disable once UnusedMember.Local
+    private readonly string testDirectoryPath =
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            @"myFiles\tests\RiderProjects\AiSuite");
+    #endif
+
+    private readonly AppVersionInfo appVersionInfo = new();
+
+    public MainWindowViewModel()
+    {
+        SetupDummyData();
+    }
+
+    public string Title => appVersionInfo.Title;
+
+    [Conditional("DEBUG")]
+    private void SetupDummyData()
+    {
+    }
+}
